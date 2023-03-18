515a3297920cc5bc3fe3483ccecd0951d6fae482
Nabil Hadi <napilfadl@gmail.com>
2023-03-18 11:11:06 +0200

    Add education and experience fields

--- src/components/CV.js
+++ src/components/CV.js
@@ -23,11 +23,11 @@ export class CV extends Component {
             <div>
               <h3 className="dot">Contacts</h3>
               <p className="cv__view__personal__email">
-                <i class="fa-solid fa-envelope fa-lg"></i>
+                <i className="fa-solid fa-envelope fa-lg"></i>
                 {this.props.personalDetailsFields.emailField}
               </p>
               <p className="cv__view__perosnal__phone-number">
-                <i class="fa-solid fa-phone fa-lg"></i>
+                <i className="fa-solid fa-phone fa-lg"></i>
                 {this.props.personalDetailsFields.phoneNumberField}
               </p>
             </div>
@@ -37,6 +37,47 @@ export class CV extends Component {
           <div className="cv__view__right__about">
             <h3 className="dot">About Me</h3>
             <p>{this.props.personalDetailsFields.aboutField}</p>
+          </div>
+
+          <div className="cv__view__education">
+            <h3 className="dot">Education</h3>
+            <ul className="cv__view__education__list">
+              {Object.keys(this.props.educationalFields).map((key) => {
+                const obj = this.props.educationalFields[key];
+                return (
+                  <li key={key}>
+                    <h4 className="sm-dot">{obj.degreeNameField}</h4>
+                    <p>{obj.schoolNameField}</p>
+                    <div>
+                      <span>{obj.eduStartDateField}</span>
+                      {" - "}
+                      <span>{obj.eduEndDateField}</span>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div className="cv__view__experience">
+            <h3 className="dot">Experience</h3>
+            <ul className="cv__view__experience__list">
+              {Object.keys(this.props.experienceFields).map((key) => {
+                const obj = this.props.experienceFields[key];
+                return (
+                  <li key={key}>
+                    <h4 className="sm-dot">{obj.positionTitleField}</h4>
+                    <h5>{obj.companyNameField}</h5>
+                    <p>{obj.descriptionField}</p>
+                    <div>
+                      <span>{obj.expStartDateField}</span>
+                      {" - "}
+                      <span>{obj.expEndDateField}</span>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </section>
       </div>
